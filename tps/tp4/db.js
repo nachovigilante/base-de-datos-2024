99f6a651e27bd52f9de800bde9e0aabfed438003
nachovigilante <nachovigilante@gmail.com>
2024-06-23 21:27:31 -0300

    Fix

--- tps/tp4/db.js
+++ tps/tp4/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
 
-export const conn = mysql.createConnection({
+export const conn = await mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "spoticfy"
+    database: "spoticfy",
 });
